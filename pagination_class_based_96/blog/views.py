6fecafbe241cb27cff7adadb6c970feebbd8d481
mithu <nilchirkutpoison@gmail.com>
2023-10-21 01:03:39 +0600

    Pagination with class Based view-96

---
+++ pagination_class_based_96/blog/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+from .models import Post
+from django.views.generic import ListView, DetailView
+from django.http import Http404
+
+# Create your views here.
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html'
+    ordering = ['id']
+    paginate_by = 3
+    paginate_orphans = 1
+
+    def get_context_data(self, *args, **kwargs):
+        try:
+            return super(PostListView, self).get_context_data(*args, **kwargs)
+        except Http404:
+            self.kwargs['page'] = 1
+            return super(PostListView, self).get_context_data(*args, **kwargs)
+
+
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/post.html'
